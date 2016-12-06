f9ba245d051adf1d81b0b76fe23c8c5a72491a12
Joao Paulo Dias <jpdias1997@hotmail.com>
2016-12-06 00:58:54 -0200

    Alteração de Atributos
    Alterei alguns atributos segundo às informações da professora. Com o tempo de desenvolvimento, vemos se vai haver mais necessidades

--- src/sistemateatro/Espetaculo.java
+++ src/sistemateatro/Espetaculo.java
@@ -3,14 +3,14 @@ import java.io.*;
 import java.util.Date;
 import java.util.LinkedList;
 public class Espetaculo {
-	private int _idEspetaculo;
-	private String _nome;
-	private String _descricao;
-	private int _faixaEtaria;
-	private int _duracaoMinutos;
+	private int idEspetaculo;
+	private String nome;
+	private String descricao;
+	private int faixaEtaria;
+	private int duracaoMinutos;
 	private static int _genID = 0;
-	public Temporada _fk_Temporada;
-	public TipoEspetaculo _fk_TipoDeEspetaculo;
-	public LinkedList<Artista> _fk_Artista = new LinkedList<Artista>();
-	public Apresentacao _apresentacao;
+	public Temporada fk_Temporada;
+	public TipoEspetaculo fk_TipoDeEspetaculo;
+	public LinkedList<Artista> fk_Artista = new LinkedList<Artista>();
+	public LinkedList<Apresentacao> fk_apresentacao = new LinkedList<Apresentacao>();
 }
