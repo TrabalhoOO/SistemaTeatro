e05141186f4fc9958961222d7c426a5bdd54367d
Joao Paulo Dias <jpdias1997@hotmail.com>
2016-12-12 00:35:24 -0200

    Compra Dao continuaçao

--- src/sistemateatro/Preferencia.java
+++ src/sistemateatro/Preferencia.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.LinkedList;
 public class Preferencia {
 	private int idPreferencia;
-	private static int genID = 0;
 	public TipoEspetaculo fk_TipoDeEspetaculo;
 	public LinkedList<Pessoa> fk_Pessoa;
 }
